b1998239c95b54c01dc7e1d61b7f2d0d7369dd24
beshirr <youssefahmedbeshir@gmail.com>
2025-05-08 18:37:22 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.blanza {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.blanza to javafx.fxml;
+    exports com.example.blanza;
+}
