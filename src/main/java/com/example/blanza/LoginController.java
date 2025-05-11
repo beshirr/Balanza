5734ac0c5100cb5ecd25fb332f2bb1e3240d4451
beshirr <youssefahmedbeshir@gmail.com>
2025-05-11 09:19:38 +0300

    gitignore, deleted unnecessary code

--- src/main/java/com/example/blanza/LoginController.java
+++ src/main/java/com/example/blanza/LoginController.java
@@ -19,27 +19,6 @@ public class LoginController {
     @FXML private PasswordField passwordField;
 
     @FXML
-    private void handleLogin(ActionEvent event) {
-        String email = emailField.getText();
-        String password = passwordField.getText();
-        if (UserManager.loginProcess(email, password)) {
-            try {
-                Parent root = FXMLLoader.load(getClass().getResource("index.fxml"));
-                Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-                stage.setScene(new Scene(root));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText("Incorrect email or password");
-            alert.showAndWait();
-        }
-    }
-    @FXML
     private void handleLogin(javafx.event.ActionEvent actionEvent) throws IOException {
         String email = emailField.getText();
         String password = passwordField.getText();
